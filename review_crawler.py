2d1f9a7afbf7ccdba887cce0f18b2808c6ac2d18
rtso <rtso489@gmail.com>
2017-03-30 00:59:35 -0500

    Put URLs in queue to be scraped
    Fill queue with all the review URLs first, then crawl for reviews
    using URLs from the queue. Removed use of status.txt to track status
    of the crawl. Switched to Review objects instead of dictionaries to hold
    review data.

--- review_crawler.py
+++ review_crawler.py
@@ -1,47 +1,116 @@
 from lxml import html
-import json, requests, random
+import simplejson
+import requests
+import random
+import _pickle
+import sys
+import os
+from collections import deque
 from time import sleep
+
+
+class Review:
+    def __init__(self, rating, title, body, helpful):
+        self.rating = rating
+        self.title = title
+        self.body = body
+        self.helpful = helpful
+
 
 with open('useragent-strings.txt', 'r') as file:
     useragent_strings = file.read().split('\n')
 
+reviews_list = deque()
+urls = deque()
 
-def parse_review(asin):
+
+def add_asin(asin):
+    print(asin)
+    # Add URLs to the queue for scraping
+    page = requests.get('https://www.amazon.com/product-reviews/' + asin,
+                        headers = {'User-Agent': random.choice(useragent_strings)})
+    parser = html.fromstring(page.content)
+    num_pages = parser.xpath('//li[@data-reftag="cm_cr_arp_d_paging_btm" or'
+                             '@data-reftag="cm_cr_getr_d_paging_btm"][last()]/a/text()')
+    if num_pages:
+        num_pages = int(num_pages[0].replace(',', ''))
+    else:
+        # There is only one page of reviews
+        num_pages = 1
+    for i in range(num_pages):
+        urls.append('https://www.amazon.com/product-reviews/' + asin + '?pageNumber=' + str(i))
+
+
+def get_reviews():
+    num_reviews = 0
     try:
-        # Add a random user agent
-        page = requests.get('https://www.amazon.com/product-reviews/' + asin, headers = {'User-Agent': random.choice(useragent_strings)})
-        parser = html.fromstring(page.content)
-        num_pages = int(parser.xpath('//li[@data-reftag="cm_cr_arp_d_paging_btm"][last()]/a/text()')[0])
-        print('num pages: ', num_pages)
-        reviews_list = []
-        for i in range(num_pages):
-            page = requests.get('https://www.amazon.com/product-reviews/' + asin + '?pageNumber=' + str(i))
+        while urls:
+            url = urls[0]
+            rl = deque()
+            page = requests.get(url, headers = {'User-Agent': random.choice(useragent_strings)})
             parser = html.fromstring(page.content)
             reviews = parser.xpath('//*[contains(@id, "customer_review")]')
+            if not reviews:
+                raise ValueError('No reviews found on ' + url)
             for review in reviews:
-                info_dict = {}
                 rating = review.xpath('.//i[@data-hook="review-star-rating"]//text()')[0]
                 title = review.xpath('.//a[@data-hook="review-title"]//text()')[0]
                 body = review.xpath('.//span[@data-hook="review-body"]//text()')[0]
                 helpful = review.xpath('.//span[@data-hook="helpful-vote-statement"]//text()')
-                if helpful:
-                    helpful = helpful[0]
+                if not helpful:
+                    helpful = 0
+                elif 'One' in helpful[0]:
+                    helpful = 1
                 else:
-                    helpful = ''
-                info_dict['rating'] = rating
-                info_dict['title'] = title
-                info_dict['body'] = body
-                info_dict['helpful'] = helpful
-                reviews_list.append(info_dict)
-        return reviews_list
+                    helpful = int(list(filter(str.isdigit, helpful[0]))[0])
+                review_obj = Review(rating[0], title, body.replace('\n', ''), helpful)
+                rl.append(review_obj)
+                num_reviews += 1
+            print('Got ', num_reviews, ' reviews')
+            urls.popleft()
+            reviews_list.extend(rl)
     except:
-        print("Error")
+        e = sys.exc_info()[0]
+        print(e)
+        print(e.args)
+        print('Stopped at ', urls[0])
+        with open('queue.p', 'wb') as url_queue_file:
+            _pickle.dump(urls, url_queue_file)
 
 
 def main():
-    test = parse_review('B0011FY9AU')
-    with open("json_file.json", "w") as json_file:
-        json.dump(test, json_file)
+    asin_filename = sys.argv[1]
+    supplement_name = asin_filename[6:-10]
+    print(supplement_name)
+    pkl_filename = 'reviews/' + supplement_name + '-reviews.p'
+    url_queue_filename = 'queue.p'
+
+    # Load reviews list
+    if os.path.exists(pkl_filename):
+        global reviews_list
+        with open(pkl_filename, 'rb') as pkl_file:
+            reviews_list = _pickle.load(pkl_file, encoding='latin1')
+        print('Loaded pkl file')
+
+    # Load URL queue
+    if os.path.exists(url_queue_filename):
+        global urls
+        with open (url_queue_filename, 'rb') as url_queue_file:
+            urls = _pickle.load(url_queue_file, encoding='latin1')
+        print('Loading ASIN queue')
+    else:
+        with open(asin_filename, 'r') as asin_file:
+            for asin in asin_file:
+                add_asin(asin.strip())
+        print('ASINs loaded in queue')
+
+    get_reviews()
+
+    with open(pkl_filename, 'wb') as pkl_file:
+        _pickle.dump(reviews_list, pkl_file)
+    with open('reviews/' + supplement_name + '-reviews.json', 'w') as json_file:
+        simplejson.dump([r.__dict__ for r in list(reviews_list)], json_file)
+    print(len(reviews_list))
 
 if __name__ == '__main__':
     main()
